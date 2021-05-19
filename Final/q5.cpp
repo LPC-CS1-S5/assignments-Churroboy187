ce250b87983506c50a9627df082b3ab051ca841c
Churroboy187 <>
2021-05-19 18:05:41 +0000

    adding in the give code you gave us from the final

---
+++ Final/q5.cpp
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+const unsigned short N=5;
+struct Scores{     
+      double sc[N];     
+      string grade;
+};
+
+struct Grade{    
+      string first;    
+      string last;   
+      string ssn;   
+      Scores score;
+};
+
+Grade g;
